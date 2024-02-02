6df451404ba12703b91f09e929f6cc1bfc13b3a9
RautMaya <suryawanshimaya28@gmail.com>
2024-02-02 13:35:37 +0530

    Prctice program

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_number,secound_number,add;
+    cout<<"enter two integer numbers"<<endl;
+    cin>>first_number>>secound_number;
+        {
+            add=first_number+secound_number;
+            cout<<first_number<<"+"<<secound_number<<"="<<add;
+
+           return 0;
+
+        }
+    }
+
+
+
