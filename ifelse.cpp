207ac11e8c5fc39119d237660e2fd7a39f4af511
RautMaya <suryawanshimaya28@gmail.com>
2024-02-04 16:23:03 +0530

    Proramming in C++

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter an integer number:";
+   std:: cin>> x;
+    if(x%2==0)
+    cout<<x<<"is even number";
+    else
+    cout<<x<< "is odd number";
+return 0;
+}
+
