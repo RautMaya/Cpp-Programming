48ea6832e72fc062098f60ce802a07e9405dbf99
RautMaya <suryawanshimaya28@gmail.com>
2024-03-21 19:49:06 +0530

    Practice Program

---
+++ Basic Program/Multiplication_of_Two_Integer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_number,secound_number,Multiplication;
+
+    cout<<"Enter Two integer:";
+    cin>>first_number>>secound_number;
+
+    Multiplication=first_number*secound_number;
+
+    cout<<first_number<<"*"<<secound_number<<"="<<Multiplication;
+
+
+    return 0;
+}
+
