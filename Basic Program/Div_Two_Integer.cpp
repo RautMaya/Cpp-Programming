be01840bc6338d2e196d975d6a11dc61c3c00a7b
RautMaya <suryawanshimaya28@gmail.com>
2024-03-21 19:50:00 +0530

    Practice Program

---
+++ Basic Program/Div_Two_Integer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_number,secound_number,Division;
+
+    cout<<"Enter Two integer:";
+    cin>>first_number>>secound_number;
+
+    Division=first_number/secound_number;
+
+    cout<<first_number<<"/"<<secound_number<<"="<<Division;
+
+
+    return 0;
+}
+
