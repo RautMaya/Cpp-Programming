631bdd6864dfd1db8c08fc1bd40012593847adb9
RautMaya <suryawanshimaya28@gmail.com>
2024-04-03 20:15:53 +0530

    Practice Program

---
+++ Pattern Printing/Print_Inverted_Full_Pyramid.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5,i,j,k;
+    for(i=n;i>=1;--i)
+    {
+        for(k=0;k<n-1;++k)
+            cout<< " ";
+        {
+            for(j=i;j<=2*i-1;++j)
+                cout<<"*";
+            {
+                for(j=0;j<i-1;++j)
+                cout<<"*";
+                {
+
+                }
+            }
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
