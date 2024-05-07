f3fbd6bd2ba6ba4b80585325cec04c35c803db65
RautMaya <suryawanshimaya28@gmail.com>
2024-05-07 22:18:39 +0530

    Practice Program

---
+++ Function Calling Techniques/call by value.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+void value(int);
+
+int main()
+{
+    int No = 10;
+    cout<< No << endl;
+
+    value(No);
+
+    cout<< No << endl;
+
+    getch();
+    return 0;
+}
+
+void value(int i)
+{
+    cout<< "Inside Function....."<< endl;
+    i++;
+}
+
