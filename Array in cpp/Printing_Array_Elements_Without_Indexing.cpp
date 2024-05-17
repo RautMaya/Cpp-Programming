92e54b834274f5d18ae337a55e8d7f8b41141b0d
RautMaya <suryawanshimaya28@gmail.com>
2024-05-17 13:45:25 +0530

    Practice Program

---
+++ Array in cpp/Printing_Array_Elements_Without_Indexing.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={11,22,33,44};
+
+   cout<<"First element:"<<*arr<<endl;
+   cout<<"second element:"<<*(arr+1)<<endl;
+   cout<<"Third element:"<<*(arr+2)<<endl;
+   cout<<"Forth element:"<<*(arr+3)<<endl;
+
+     return 0;
+
+}
