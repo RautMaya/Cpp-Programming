cca21b3786de6f11bfc20ce7d5fdfb9e31ad8df9
RautMaya <suryawanshimaya28@gmail.com>
2024-05-19 12:27:18 +0530

    Practice Program

--- Looping Program/continue.cpp
+++ Looping Program/continue.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 int main()
 {
-    for(int i=1;i<=5;i++)
+    for(int i=0;i<=10;i++)
     {
         if(i==3)
         {
-            break;
+            continue;
         }
-        cout<<i<<endl;
+        cout<<i<<"\n";
     }
     return 0;
 
 }
+
+
