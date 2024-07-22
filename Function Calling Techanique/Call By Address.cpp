73ae94fb4fa823948843c3c3bbe257aac1d0826f
RautMaya <suryawanshimaya28@gmail.com>
2024-07-22 13:01:16 +0530

    Practice Program

---
+++ Function Calling Techanique/Call By Address.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void Address(int*);
+
+ int main()
+ {
+     int No=10;
+     cout<<No<<endl;
+
+     Address(&No);
+
+     cout<<No<<endl;
+
+     getch();
+     return 0;
+ }
+ void Address(int *P)
+ {
+     cout<<"Inside Function"<<endl;
+     (*P)++;
+ }
+
