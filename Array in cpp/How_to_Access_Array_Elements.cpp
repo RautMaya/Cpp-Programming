d663c5a78bb60ffa54ae5e1fc9605ca74c94a577
RautMaya <suryawanshimaya28@gmail.com>
2025-01-02 17:52:45 +0530

    Practice Program

--- Array in cpp/How_to_Access_Array_Elements.cpp
+++ Array in cpp/How_to_Access_Array_Elements.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 int main()
 {
-    int arr[3];
+    int arr[4];
 
     arr[0]=10;
     arr[1]=20;
     arr[2]=30;
+    arr[3]=40;
 
     cout<<"arr[0]:"<<arr[0]<<endl;
     cout<<"arr[1]:"<<arr[1]<<endl;
     cout<<"arr[2]:"<<arr[2]<<endl;
+    cout<<"arr[3]:"<<arr[3]<<endl;
 
     return 0;
 }
